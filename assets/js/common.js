eb99cf1d7cc244daa74a66630f54409300c1c8b0
Damir <damir.dmr88@gmail.com>
2021-12-04 15:46:02 +0600

    add page 2 & tabs

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,24 @@
 $('.slider-wrap').owlCarousel({
   items: 1,
   nav: true,
-  navText : ["<i class='arr-left'></i>","<i class='arr-right'></i>"]
+  navText : ["<i class='arr-left'></i>","<i class='arr-right'></i>"],
+  autoplay: true,
+  autoplayTimeout: 5000
 });
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
